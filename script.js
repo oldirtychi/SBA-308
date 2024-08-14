4aef9bf99284d590ffc6a296824869adbd8b15f4
oldirtychi <ejsapayanp@gmail.com>
2024-08-13 23:36:57 -0500

    wimm

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
+// {
+//     // the ID of the learner for which this data has been collected
+//     "id": number,
+//     // the learner’s total, weighted average, in which assignments
+//     // with more points_possible should be counted for more
+//     // e.g. a learner with 50/100 on one assignment and 190/200 on another
+//     // would have a weighted average score of 240/300 = 80%.
+//     "avg": number,
+//     // each assignment should have a key with its ID,
+//     // and the value associated with it should be the percentage that
+//     // the learner scored on the assignment (submission.score / points_possible)
+//     <assignment_id>: number,
+//     // if an assignment is not yet due, it should not be included in either
+//     // the average or the keyed dictionary of scores
+// }
+
+
+// OBJECTIVE: Create a function named getLearnerData() that accepts these values as 
+// parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), 
+// and returns the formatted result, which should be an array of objects as described 
+// above
+
+
+function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
+  if (courseInfo.id !== assignmentGroup.course_id) {
+    throw new Error("Invalid Course!");
+  }
+
+  //calculate grade percentage
+  function calculatePercentage(score, pointsPossible) {
+    if (pointspossible === 0) {
+      throw new Error("Grade cannot be 0");
+    }
+return (score / pointsPossible) * 100;
+  }
+  //to check if assignment was turned in late
+  function isPastDue(dueDate, submittedDate) {
+    return new Date(submittedDate) > new Date(dueDate);
+  }
+
+  const learnerData = {};
+
+  learnerSubmissions.forEach(submission => {
+    const { learner_id, assignment_id, submission: {submitted_at, score}} = submission;
+    const assignment = assignmentGroup.assignments.find(a => a.id === assignment_id);
+
+    if (!assignment) {
+      throw new Error('Assignment ID ${assignment_ID} not found');
+    }
+
+    const dueDate = assignment.due_at || assignment.due_on;
+    if (new Date(dueDate) > new Date()) {
+      return;
+    }
+
+    let finalScore = score;
+    if (isPastDue(dueDate, submitted_at)) {
+      finalScore -= assignment.points_possible * 0.1;
+    }
+
+    if  (!learnerData[learner_id]) {
+      learnerData[learner_id] = { id: learner_id, avg: 0, totalpoints: 0, totalposssible: 0};
+
+    }
+
+    const percentage = calculatePercentage(finalScore, assignment.points_possible);
+    learnerData[learner_id][assignment_id] = percentage;
+    learnerData[learner_id].totalpoints += finalScore;
+    learnerData[learner_id].totalPossible += assignment.points_possible;
+
+  }
+);
+
+//calculate the average points for each learner
+return Object.values(learnerData).map(learner => {
+  learner.avg= (learner.totalPoints / learner.totalPossible) * 100;
+  delete learner.totalPoints;
+  delete learner.totalPossible;
+  return learner;
+  }
+);
+}
+
 // A CourseInfo object, which looks like this:
 const courseInfo = {
   id: 101,
@@ -76,28 +159,4 @@ const learnerSubmissions = [
   }
 ];
 
-
-
-
-
-// {
-//     // the ID of the learner for which this data has been collected
-//     "id": number,
-//     // the learner’s total, weighted average, in which assignments
-//     // with more points_possible should be counted for more
-//     // e.g. a learner with 50/100 on one assignment and 190/200 on another
-//     // would have a weighted average score of 240/300 = 80%.
-//     "avg": number,
-//     // each assignment should have a key with its ID,
-//     // and the value associated with it should be the percentage that
-//     // the learner scored on the assignment (submission.score / points_possible)
-//     <assignment_id>: number,
-//     // if an assignment is not yet due, it should not be included in either
-//     // the average or the keyed dictionary of scores
-// }
-
-
-// OBJECTIVE: Create a function named getLearnerData() that accepts these values as 
-// parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), 
-// and returns the formatted result, which should be an array of objects as described 
-// above
+console.log(getLearnerData(courseInfo, assignmentGroup, learnerSubmissions));
